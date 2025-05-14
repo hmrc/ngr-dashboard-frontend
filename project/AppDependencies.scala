7d011c5503f4f7a3ca16e53f5faf24b15bd9f93f
anna-shen-hmrc <anna-shen-hmrc@users.noreply.github.com>
2025-05-14 15:33:22 +0100

    NGR-614: tidy up

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,7 +1,5 @@
-import play.core.PlayVersion
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
-import sbt._
+import play.sbt.PlayImport.*
+import sbt.*
 
 object AppDependencies {
 
@@ -10,16 +8,15 @@ object AppDependencies {
   
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "12.0.0",
-    "uk.gov.hmrc"             %% "centralised-authorisation-resource-client-play-30" % "1.4.0",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"                        % bootstrapVersion,
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"                        % "12.0.0",
+    "uk.gov.hmrc"             %% "centralised-authorisation-resource-client-play-30" % "1.5.0",
     "uk.gov.hmrc"             %% "domain-play-30"                                    % "11.0.0",
     "com.beachape"            %% "enumeratum-play"                                   %  enumeratumVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-    
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion    % Test,
     "org.jsoup"               %  "jsoup"                      % "1.13.1"            % Test,
   )
 
