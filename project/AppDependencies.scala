b16deed93919ab0bb1e7ca2819bb6e65ed262c05
Yuriy Tumakha <tumakha@gmail.com>
2025-10-16 15:34:08 +0100

    NGR-1830 centralised-authorisation-resource-client-play-30 1.12.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"                        % bootstrapVersion,
     "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"                        % "12.17.0",
-    "uk.gov.hmrc"             %% "centralised-authorisation-resource-client-play-30" % "1.13.0",
+    "uk.gov.hmrc"             %% "centralised-authorisation-resource-client-play-30" % "1.12.0",
     "uk.gov.hmrc"             %% "domain-play-30"                                    % "11.0.0",
     "com.beachape"            %% "enumeratum-play"                                   %  enumeratumVersion
   )
